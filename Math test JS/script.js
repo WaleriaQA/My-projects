b1b5ad523ab342e276047d2b20d3f4ff9ea08a0c
WaleriaQA <86474410+WaleriaQA@users.noreply.github.com>
2022-12-14 09:47:34 +0200

    Add files via upload

---
+++ Math test JS/script.js
@@ -0,0 +1,39 @@
+let button = document.getElementById("finish");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+
+    let result = 0;
+
+    let a1 = document.getElementById('q1').value;
+    if (a1 === '4') {
+        result++;
+    }
+
+    let a2 = document.getElementById('q2').value;
+    if (a2 === '6') {
+        result++;
+    }
+
+    let a3 = document.getElementById('q3').value;
+    if (a3 === '10') {
+        result++;
+    }
+
+    let a4 = document.getElementById('q4').value;
+    if (a4 === '-1') {
+        result++;
+    }
+
+    let a5 = document.getElementById('q5').value;
+    if (a5 === '1') {
+        result++;
+    }
+
+    let a6 = document.getElementById('q6').value;
+    if (a6 === '20') {
+        result++;
+    }
+    alert('Number of correct answers: ' + result);
+}
